ea4e660ce11069a752c4320f9371618394b33a25
cruZconsultingGroup <auriopinto2000@gmail.com>
2020-08-22 20:38:12 +0800

    main_page

---
+++ precache-manifest.260793c90f0114e0833b616192b65b0f.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "f749ac315d709722968dfff229b7ebc9",
+    "url": "/img/theme/Pinto.jpg"
+  },
+  {
+    "revision": "9e8fbed0cfddbe2c1e6c054bd6ad8fd1",
+    "url": "/img/theme/center.jpg"
+  },
+  {
+    "revision": "d95c38fb59b8c215d39f",
+    "url": "/js/chunk-vendors.23709363.js"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "aca5b095b7d9fd92961afbee95949ab6",
+    "url": "/img/theme/city.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "3e0d27d61f05ed14e02a2aa169cc2b79",
+    "url": "/img/theme/cruz.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "1a14d12886d336aa031949a7002c68f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "a441a0e2f77afe1d401c",
+    "url": "/js/app.e5b26376.js"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "1eabced0a5de2dd82b70336711abe76c",
+    "url": "/img/brand/cruz.png"
+  },
+  {
+    "revision": "81dab98b5812f735b869972a439fc46b",
+    "url": "/img/brand/pw.shanghai.jpg"
+  },
+  {
+    "revision": "fbb34fe57ef28ff58773d7c22471b7f7",
+    "url": "/img/brand/pweb.png"
+  },
+  {
+    "revision": "f8d66635d36a5b2ef698e30fb5f18aca",
+    "url": "/img/theme/auriocn.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "59106dc7fc87a1dbb6b6848b036042e8",
+    "url": "/img/clogo.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "f8d66635d36a5b2ef698e30fb5f18aca",
+    "url": "/img/brand/auriocn.png"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "a441a0e2f77afe1d401c",
+    "url": "/css/app.83f4993f.css"
+  }
+];
