070e4820cd3b514c120d4a712e1c89e1ed6bcb0e
cruZconsultingGroup <auriopinto2000@gmail.com>
2020-09-07 02:47:33 +0800

    another page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <!-- <span>This is cs50</span> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scope lang="scss">
+// .navbar {
+//   background-image: url("/src/assets/layourt.png");
+// }
+</style>
---
+++ src/views/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="unix">
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar";
+export default {
+  name: "index",
+  components: { Navbar },
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+.unix {
+  //   background-color: aqua;
+  background-image: url("../assets/layourt.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  //   min-height: 100%;
+  min-height: 100vh;
+}
+</style>
