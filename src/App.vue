4821e7b8f088b1d9f58920b0b2fdf70a65246d40
cruZconsultingGroup <auriopinto2000@gmail.com>
2020-09-06 23:08:18 +0800

    ipaasolution

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-titlle>IPAA-Solution</v-toolbar-titlle>
+
+       <v-text-field
+            placeholder="Search"
+            class="ml-8"
+            style="max-width:350px"
+            prepend-inner-icon="mdi-magnify"
+            outlined
+            rounded
+            dense
+            hide-details
+
+          ></v-text-field>
+
+      <v-spacer />
+      <v-btn>Create Event</v-btn>
+    </v-app-bar>
+    <!-- <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Aurio Pinto</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar> -->
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="6" offset-sm="3">
+        <v-tabs v-model="tab" grow>
+          <v-tab>Item One</v-tab>
+          <v-tab>Item Two</v-tab>
+          <v-tab>Item Three</v-tab>
+        </v-tabs>
+
+        <!-- <v-card class="mx-auto" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text>
+              Share
+            </v-btn>
+
+            <v-btn color="orange" text>
+              Explore
+            </v-btn>
+          </v-card-actions>
+        </v-card> -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    tab: 0,
+    watch: {
+      tab(val) {
+        if (this.tab === 0) {
+          this.showAllEvents();
+        } else {
+          this.showEventsByTypes();
+        }
+      },
+    },
+
+    // ecosystem: [
+    //   {
+    //     text: 'vuetify-loader',
+    //     href: 'https://github.com/vuetifyjs/vuetify-loader',
+    //   },
+    //   {
+    //     text: 'github',
+    //     href: 'https://github.com/vuetifyjs/vuetify',
+    //   },
+    //   {
+    //     text: 'awesome-vuetify',
+    //     href: 'https://github.com/vuetifyjs/awesome-vuetify',
+    //   },
+    // ],
+    // importantLinks: [
+    //   {
+    //     text: 'Documentation',
+    //     href: 'https://vuetifyjs.com',
+    //   },
+    //   {
+    //     text: 'Chat',
+    //     href: 'https://community.vuetifyjs.com',
+    //   },
+    //   {
+    //     text: 'Made with Vuetify',
+    //     href: 'https://madewithvuejs.com/vuetify',
+    //   },
+    //   {
+    //     text: 'Twitter',
+    //     href: 'https://twitter.com/vuetifyjs',
+    //   },
+    //   {
+    //     text: 'Articles',
+    //     href: 'https://medium.com/vuetify',
+    //   },
+    // ],
+    // whatsNext: [
+    //   {
+    //     text: 'Explore components',
+    //     href: 'https://vuetifyjs.com/components/api-explorer',
+    //   },
+    //   {
+    //     text: 'Select a layout',
+    //     href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+    //   },
+    //   {
+    //     text: 'Frequently Asked Questions',
+    //     href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+    //   },
+    // ],
+  }),
+  methods: {
+    showAllEvents() {
+      console.log("west");
+    },
+    showEventsByTypes() {
+      console.log("server");
+    },
+  },
+};
+</script>
